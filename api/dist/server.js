f5e4592fddf8d7de8d8f623805cd2571103b534d
Yaman Sharma <iamymnsha@github.com>
2025-07-24 21:34:35 +0530

    Added nodemon in backend for automatic restart

--- api/dist/server.js
+++ api/dist/server.js
@@ -3,19 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// Import the 'express' module
 const express_1 = __importDefault(require("express"));
-// Create an Express application
 const app = (0, express_1.default)();
 // Set the port number for the server
 const port = 3000;
-// Define a route for the root path ('/')
 app.get('/', (req, res) => {
-    // Send a response to the client
-    res.send('Hello, TypeScript + Node.js + Express!');
+    res.send('Hello from Backend');
 });
-// Start the server and listen on the specified port
+// Start the server and listen on Port
 app.listen(port, () => {
-    // Log a message when the server is successfully running
     console.log(`Server is running on http://localhost:${port}`);
 });
