156a89f973394889488cc74b3bee667d1a237ce0
Yaman Sharma <iamymnsha@github.com>
2025-07-24 22:13:53 +0530

    Added env for api using dontenv, added hot refresh with ts-node-dev and scripts for prod

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config({ path: '.env.local'});
+
 const app = express();
 
+console.log(process.env.PORT); // Log the port to verify it's loaded correctly
+
 // Set the port number for the server
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Backend');
+  res.send('Hello from Backen');
 });
 
 // Start the server and listen on Port
